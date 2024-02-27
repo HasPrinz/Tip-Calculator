51fbba813088520d51536d7e71a471ddd547ba11
LichtCode <prinzdarey@gmail.com>
2024-02-27 15:14:06 +0100

    added a tip calculator

---
+++ index.js
@@ -0,0 +1,20 @@
+const btnEl = document.getElementById("calculate");
+const billEL = document.getElementById("bill");
+const tipEL = document.getElementById("tip");
+const totalEL = document.getElementById("total");
+
+
+function calculateTotal(){
+    // const billValue = billEl.value;
+    // const tipValue = tipEl.value;
+    // console.log(billValue);
+
+    // const totalValue = billValue * (1 + tipValue / 100);
+    
+
+    console.log("clicked");
+
+
+}
+
+btnEl.addEventListener("click", calculateTotal);
